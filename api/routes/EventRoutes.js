4cdcc4127a7539f1ed8b8e8966187a279dfd19a0
Sebassllr <Sebaslunasllr@gmail.com>
2018-11-20 20:25:13 -0500

    Se organizan metodo para cambiar la petición a get

--- api/routes/EventRoutes.js
+++ api/routes/EventRoutes.js
@@ -7,10 +7,10 @@ router.get('/evento/', rol.findAll);
 router.get('/propuesta/', rol.findAllProps);
 router.get('/getByCategory/', rol.findByCategory);
 router.get('/getByCategoryByEventId/', rol.findCharacteristicByEventId);
+router.get('/evento/findFinalizedEvents', rol.findAllFinalizeEvents);
 router.post('/evento/', rol.create);
 router.post('/evento/Vote', rol.updateVote);
 router.post('/evento/FinalVote', rol.closeVotation);
 router.post('/evento/updateEvent', rol.updateEvent);
-router.post('/evento/findFinalizedEvents', rol.findAllFinalizeEvents);
 
 module.exports = router
